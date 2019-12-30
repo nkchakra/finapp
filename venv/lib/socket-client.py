cb5acc4c28d724cad42050396fd885d2a32db721
Nikhil <nsc27@case.edu>
2019-12-29 18:26:22 -0800

    socket trial

---
+++ venv/lib/socket-client.py
@@ -0,0 +1,11 @@
+import socket
+
+HOST = '50.18.144.216'  # The server's hostname or IP address
+PORT = 5000        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+
+print('Received', repr(data))
